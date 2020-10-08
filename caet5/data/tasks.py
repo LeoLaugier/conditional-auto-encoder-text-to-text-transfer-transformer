bbab54293709dd4d77f8ca1eda35524f4e253b7a
Leo Laugier <leojlaugier@gmail.com>
2020-10-08 11:17:04 +0200

    Added rail dataset

--- caet5/data/tasks.py
+++ caet5/data/tasks.py
@@ -341,3 +341,65 @@ MixtureRegistry_ll.add(
         [task_name],
         default_rate=1.0
     )
+
+
+# ======================== Rail ==================================
+task_name = "rail"
+
+splits_raw = ["train", "dev", "test"]
+splits = ["train", "validation", "test"]
+
+metric_fns_task = metric_fns.copy()
+
+output_features = ["inputs", "targets", "attribute", "codeprefixedtargets", "controlcode"]
+
+if FLAGS.data_dir_name:
+    data_dir = os.path.join(FLAGS.base_dir, FLAGS.data_dir_name)
+else:
+    data_dir = os.path.join(FLAGS.base_dir, "data_tsv_%s" % task_name)
+
+dataset_tsv_path = {
+            "train": os.path.join(data_dir, "%s-train.tsv" % task_name.lower()),
+            "validation": os.path.join(data_dir, "%s-validation.tsv" % task_name.lower()),
+            "test": os.path.join(data_dir, "%s-test.tsv" % task_name.lower())
+        }
+
+tsvs_exist = [tf.io.gfile.exists(dataset_tsv_path[split]) for split in splits]
+
+for i, tsv_exists in enumerate(tsvs_exist):
+    split = splits[i]
+    split_raw = splits_raw[i]
+    if not tsv_exists:
+        tf.compat.v1.logging.info("Generating TSV for the %s split." % split)
+        ext = ["healthy", "unhealthy"]
+        dataset_raw_dir = os.path.join(FLAGS.base_dir, FLAGS.data_raw_dir_name)
+        in_fnames = [(1, os.path.join(dataset_raw_dir, "%s.%s" % (split_raw, ext[1]))),
+                     (0, os.path.join(dataset_raw_dir, "%s.%s" % (split_raw, ext[0])))]
+
+        raw_to_tsv(in_fnames, dataset_tsv_path[split])
+
+        tf.compat.v1.logging.info("TSV for the %s split generated." % split)
+
+
+def dataset_fn(split, shuffle_files=False):
+    fn = functools.partial(tsv_to_dataset_fn, dataset_tsv_path=dataset_tsv_path)
+    return fn(split, shuffle_files=shuffle_files)
+
+task_kwargs = {"dataset_fn": dataset_fn}
+
+TaskRegistry_ll.add(
+        task_name,
+        splits=splits,
+        text_preprocessor=[at_preprocessor],
+        sentencepiece_model_path=DEFAULT_SPM_PATH,
+        postprocess_fn=t5.data.postprocessors.lower_text,
+        metric_fns=metric_fns_task,
+        token_preprocessor=preprocessors.unsupervised,
+        output_features=output_features,
+        **task_kwargs)
+
+MixtureRegistry_ll.add(
+        "mixture_%s" % task_name,
+        [task_name],
+        default_rate=1.0
+    )
